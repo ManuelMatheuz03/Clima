95c969623bb7f1da85c97f93395ba7a39c54d963
ManuelMatheuz03 <38057638+ManuelMatheuz03@users.noreply.github.com>
2018-05-29 21:21:27 -0500

    commit

--- Clima/Clima/App.xaml.cs
+++ Clima/Clima/App.xaml.cs
@@ -5,13 +5,14 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace Clima
 {
+    using View;
 	public partial class App : Application
 	{
 		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new WeatherPage();
 		}
 
 		protected override void OnStart ()
--- Clima/Clima/ViewModel/WeatherViewModelPage.cs
+++ Clima/Clima/ViewModel/WeatherViewModelPage.cs
@@ -2,11 +2,12 @@
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
 using Xamarin.Forms;
-using System.Net.Http;
 
 namespace Clima.ViewModel
 {
     using Clima.Model;
+    using System.Net.Http;
+
     public class WeatherViewModelPage : NotificableViewModel
     {
         #region Atributos
@@ -142,11 +143,13 @@ namespace Clima.ViewModel
             UltimaActualizacion = weatherModel.Query.Results.Channel.Item.Condition.Date;
             Temperatura = weatherModel.Query.Results.Channel.Item.Condition.Temp;
             Clima = weatherModel.Query.Results.Channel.Item.Condition.Text;
+            var imgLink = $"http://l.yimg.com/a/i/us/we/52/{weatherModel.Query.Results.Channel.Item.Condition.Code}.gif";
+            Imagen = ImageSource.FromUri(new Uri(imgLink));
         }
 
         private string ObtenerURL()
         {
-            string serviceURL = $"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{ResultTerm}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys}";
+            string serviceURL = $"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{ResultTerm}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys}}";
             return serviceURL;
         }
         #endregion
